cc491e30a597c1d6b70169061988c7cc37ed716e
Karim <102773219+Kagemni@users.noreply.github.com>
2024-02-18 06:46:16 -0500

    Update digitalBuzzer.ino

--- digitalBuzzer.ino
+++ digitalBuzzer.ino
@@ -32,10 +32,93 @@ void playNote(char note, int duration) {
 void setup() {
   // put your setup code here, to run once:
   pinMode(buzzer, OUTPUT);
+
 }
 
 void getRickRolledLol() {
-  // insert rickroll stuff here
+  int a3f = 208;     // 208 Hz
+  int b3f = 233;     // 233 Hz
+  int b3 = 247;      // 247 Hz
+  int c4 = 261;      // 261 Hz MIDDLE C
+  int c4s = 277;     // 277 Hz
+  int e4f = 311;     // 311 Hz    
+  int f4 = 349;      // 349 Hz 
+  int a4f = 415;     // 415 Hz  
+  int b4f = 466;     // 466 Hz 
+  int b4 = 493;      //  493 Hz 
+  int c5 = 523;      // 523 Hz 
+  int c5s = 554;     // 554  Hz
+  int e5f = 622;     // 622 Hz  
+  int f5 = 698;      // 698 Hz 
+  int f5s = 740;     // 740 Hz
+  int a5f = 831;     // 831 Hz 
+
+  int rest = -1;
+
+  volatile int beatlength = 100; // determines tempo
+  float beatseparationconstant = 0.3;
+
+  int threshold;
+
+  int a; // part index
+  int b; // song index
+  int c; // lyric index
+
+  boolean flag;
+
+  // Parts 1 and 2 (Intro)
+
+  int song1_intro_melody[] =
+  {c5s,  e5f, e5f, f5, a5f, f5s, f5, e5f, c5s, e5f, rest, a4f, a4f};
+
+  int song1_intro_rhythmn[]  =
+  {6, 10, 6, 6, 1, 1, 1, 1, 6, 10, 4, 2, 10};
+
+  // Parts 3 or 5 (Verse 1)
+
+  int  song1_verse1_melody[] =
+  { rest, c4s, c4s, c4s, c4s, e4f, rest, c4, b3f, a3f,
+    rest, b3f, b3f, c4, c4s, a3f, a4f, a4f, e4f,
+    rest, b3f, b3f, c4, c4s, b3f,  c4s, e4f, rest, c4, b3f, b3f, a3f,
+    rest, b3f, b3f, c4, c4s, a3f, a3f, e4f,  e4f, e4f, f4, e4f,
+    c4s, e4f, f4, c4s, e4f, e4f, e4f, f4, e4f, a3f,
+    rest,  b3f, c4, c4s, a3f, rest, e4f, f4, e4f
+  };
+
+  int song1_verse1_rhythmn[] =
+  {  2, 1, 1, 1, 1, 2, 1, 1, 1, 5,
+    1, 1, 1, 1, 3, 1, 2, 1, 5,
+    1, 1, 1, 1, 1,  1, 1, 2, 1, 1, 1, 1, 3,
+    1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 4,
+    5, 1, 1, 1,  1, 1, 1, 1, 2, 2,
+    2, 1, 1, 1, 3, 1, 1, 1, 3
+  };
+
+  //  Parts 4 or 6 (Chorus)
+
+  int song1_chorus_melody[] =
+  { b4f, b4f, a4f, a4f,
+    f5, f5, e5f, b4f, b4f, a4f, a4f, e5f, e5f, c5s, c5, b4f,
+    c5s, c5s, c5s, c5s,
+    c5s, e5f, c5, b4f, a4f, a4f, a4f, e5f, c5s,
+    b4f, b4f, a4f, a4f,
+    f5,  f5, e5f, b4f, b4f, a4f, a4f, a5f, c5, c5s, c5, b4f,
+    c5s, c5s, c5s, c5s,
+    c5s, e5f, c5, b4f, a4f, rest, a4f, e5f, c5s, rest
+  };
+
+  int song1_chorus_rhythmn[]  =
+  { 1, 1, 1, 1,
+    3, 3, 6, 1, 1, 1, 1, 3, 3, 3, 1, 2,
+    1, 1, 1, 1,
+    3, 3, 3, 1, 2, 2, 2, 4, 8,
+    1, 1, 1, 1,
+    3, 3, 6, 1, 1, 1, 1, 3, 3, 3,  1, 2,
+    1, 1, 1, 1,
+    3, 3, 3, 1, 2, 2, 2, 4, 8, 4
+  };
+
+
 }
 
 void happyBirthdayGrandma() {
