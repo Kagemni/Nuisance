f2324b90fb770086b575bb5f0f6d796d53a0f28b
Karim <102773219+Kagemni@users.noreply.github.com>
2024-02-17 19:49:48 -0500

    Add files via upload

---
+++ digitalBuzzer.ino
@@ -0,0 +1,29 @@
+#define buzzerOne 12
+#define buzzerTwo 13
+
+bool canIScreamYet = false;
+
+void setup()
+{  
+  // put your setup code here, to run once:
+  pinMode(buzzerOne, OUTPUT);
+  pinMode(buzzerTwo, OUTPUT);
+}
+ 
+void loop(){
+
+  if (canIScreamYet = true) {
+  int frequencyOne = random(1000, 9001);
+  tone(buzzerOne, frequencyOne);
+  delay(1000);  // BuzzerOne plays for 1 second
+  
+  int frequencyTwo = random(1000, 9001);
+  tone(buzzerTwo, frequencyTwo);
+  delay(1000);  // 1 second
+  } else {
+  // Turn off both buzzers
+  noTone(buzzerOne);
+  noTone(buzzerTwo);
+
+  }
+}
