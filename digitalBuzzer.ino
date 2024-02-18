0a56029952bfb1ced357f440a521f01d5b12eab5
Karim <102773219+Kagemni@users.noreply.github.com>
2024-02-18 06:05:22 -0500

    Update digitalBuzzer.ino

--- digitalBuzzer.ino
+++ digitalBuzzer.ino
@@ -1,29 +1,29 @@
-#define buzzerOne 14
-#define buzzerTwo 15
+#define buzzer 8
 
-bool canIScreamYet = false;
+// bool canIScreamYet = true;
 
 void setup()
 {  
   // put your setup code here, to run once:
-  pinMode(buzzerOne, OUTPUT);
-  pinMode(buzzerTwo, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+}
+
+void getRickRolledLol(){
+  // insert rickroll stuff here
+}
+
+void happyBirthdayGrandma(){
+// happy birthday here
+
+}
+
+void wompWompNoises(){
+  // womp womp
 }
  
 void loop(){
 
-  if (canIScreamYet = true) {
-  int frequencyOne = random(1000, 9001);
-  tone(buzzerOne, frequencyOne);
-  delay(1000);  // BuzzerOne plays for 1 second
-  
-  int frequencyTwo = random(1000, 9001);
-  tone(buzzerTwo, frequencyTwo);
-  delay(1000);  // 1 second
-  } else {
   // Turn off both buzzers
-  noTone(buzzerOne);
-  noTone(buzzerTwo);
+  noTone(buzzer);
 
-  }
 }
