c45e14230b37a2f2413bf1f1ea856efadc141162
Karim <102773219+Kagemni@users.noreply.github.com>
2024-02-18 06:35:22 -0500

    Update digitalBuzzer.ino

--- digitalBuzzer.ino
+++ digitalBuzzer.ino
@@ -1,14 +1,40 @@
 #define buzzer 8
 
-// bool canIScreamYet = true;
+void playTone(int tone, int duration) {
+  for (long i = 0; i < duration * 1000L; i += tone * 2) {
+    digitalWrite(buzzer, HIGH);
+    delayMicroseconds(tone);
+    digitalWrite(buzzer, LOW);
+    delayMicroseconds(tone);
+  }
+}
 
-void setup()
-{  
+void playNote(char note, int duration) {
+  char names[] = {'C', 'D', 'E', 'F', 'G', 'A', 'B',
+                  'c', 'd', 'e', 'f', 'g', 'a', 'b',
+                  'x', 'y' };
+
+  int tones[] = { 1915, 1700, 1519, 1432, 1275, 1136, 1014,
+                   956, 834, 765, 593, 468, 346, 224,
+                   655 , 715 };
+
+  int SPEE = 5;
+
+  // play the tone corresponding to the note name
+  for (int i = 0; i < 17; i++) {
+    if (names[i] == note) {
+      int newduration = duration / SPEE;
+      playTone(tones[i], newduration);
+    }
+  }
+}
+
+void setup() {
   // put your setup code here, to run once:
   pinMode(buzzer, OUTPUT);
 }
 
-void getRickRolledLol(){
+void getRickRolledLol() {
   // insert rickroll stuff here
 }
 
@@ -29,43 +55,16 @@ void happyBirthdayGrandma() {
     delay(tempo);
   }
 }
-void playNote(char note, int duration) {
-char names[] = {'C', 'D', 'E', 'F', 'G',   'A', 'B',           
 
-                 'c', 'd', 'e', 'f', 'g', 'a', 'b',
-
-                  'x', 'y' };
-
-int tones[] = { 1915, 1700, 1519, 1432, 1275,   1136, 1014,
-
-                 956,  834,  765,  593,  468,  346,  224,
-
-                  655 , 715 };
-
-int SPEE = 5;
-
-// play the tone corresponding   to the note name
-
-for (int i = 0; i < 17; i++) {
-
-   if (names[i] ==   note) {
-    int newduration = duration/SPEE;
-     playTone(tones[i], newduration);
-    }
-}
-}
-
-void wompWompNoises(){
-  for (int freq = 1000; freq <= 9000; freq += 100){
-    tone(buzzer,freq);
+void wompWompNoises() {
+  for (int freq = 1000; freq <= 9000; freq += 100) {
+    tone(buzzer, freq);
     delay(100);
   }
   noTone(buzzer);
 }
- 
-void loop(){
 
+void loop() {
   // Turn off both buzzers
   noTone(buzzer);
-
 }
